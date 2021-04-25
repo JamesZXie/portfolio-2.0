e6a49ba4b2514c309976b4a25d90ded20ada63d4
James Xie <jamesxie2019@u.northwestern.edu>
2021-04-25 12:31:19 -0700

    Add env file to git and create random p5 sketch

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
-// import Sketch from 'react-p5';
+import Sketch from 'react-p5';
 
-const header = () => <div>test</div>;
+const header = () => {
+  const a = 300;
+  const b = 300;
+  const setup = (p5, canvasParentRef) => {
+    // Canvas of size 1000x800
+    const xyz = p5.createCanvas(1000, 800).parent(canvasParentRef);
+  };
+  const draw = (p5) => {
+    p5.background('rgb(100%,0%,10%)');
+  };
+  return (
+    <div className="App">
+      <Sketch setup={setup} draw={draw} className="App" />
+    </div>
+  );
+};
 
 export default header;
