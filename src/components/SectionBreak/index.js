6c735d382f4db9789490749338b4ced42c608153
James Xie <jamesxie2019@u.northwestern.edu>
2022-12-30 14:18:08 -0800

    Pools

--- src/components/SectionBreak/index.js
+++ src/components/SectionBreak/index.js
@@ -4,16 +4,15 @@ import {
 } from '@chakra-ui/react';
 import './section-break.scss';
 
-const colStart = [1, 7];
-const colSpan = [12, 6];
-const colEnd = [12, 12];
+const colStart = [2, 4];
+const colEnd = [12, 10];
 
 const SectionBreak = (props) => (
   <GridItem
     colStart={colStart}
-    colSpan={colSpan}
     colEnd={colEnd}
     paddingTop="1rem"
+    paddingBottom="1rem"
   >
     <Box
       className="section-break"
